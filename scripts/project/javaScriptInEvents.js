da5388bab26b5ddcb7f163f6cfeac3a8fc958882
DenysFedorenkoA <121401812+DenysFedorenkoA@users.noreply.github.com>
2025-08-07 14:43:40 +0300

    Add files via upload

---
+++ scripts/project/javaScriptInEvents.js
@@ -0,0 +1,11 @@
+
+
+const scriptsInEvents = {
+
+	async EsMain_Event86_Act1(runtime, localVars)
+	{
+		console.log("Too Little Currency: "+localVars.currency)
+	}
+};
+
+globalThis.C3.JavaScriptInEvents = scriptsInEvents;
